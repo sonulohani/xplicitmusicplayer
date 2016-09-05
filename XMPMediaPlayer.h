d348a56a41c2279543497ac64e5ca41e30d10f82
Sonu Lohani <sonulohani@gmail.com>
2016-09-05 22:08:36 +0530

    Updated licenses;

--- XMPMediaPlayer.h
+++ XMPMediaPlayer.h
@@ -1,4 +1,23 @@
-#pragma once
+/*\
+|*|  Copyright 2016-2017 Sonu Lohani <sonulohani@gmail.com>
+|*|
+|*|  This file is part of the XMPMusicPlayer program.
+|*|
+|*|  AvCaster is free software: you can redistribute it and/or modify
+|*|  it under the terms of the GNU Lesser General Public License version 3
+|*|  as published by the Free Software Foundation.
+|*|
+|*|  AvCaster is distributed in the hope that it will be useful,
+|*|  but WITHOUT ANY WARRANTY; without even the implied warranty of
+|*|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|*|  GNU Lesser General Public License for more details.
+|*|
+|*|  You should have received a copy of the GNU Lesser General Public License
+|*|  along with AvCaster.  If not, see <http://www.gnu.org/licenses/>.
+\*/
+
+#ifndef XMPMEDIAPLAYER_H
+#define XMPMEDIAPLAYER_H
 
 #include <QMediaPlayer>
 
@@ -16,3 +35,5 @@ namespace xmp
 		};
 	} //multimedia
 } // xmp
+
+#endif // XMPMEDIAPLAYER_H
--- macros.h
+++ macros.h
@@ -1,3 +1,21 @@
+/*\
+|*|  Copyright 2016-2017 Sonu Lohani <sonulohani@gmail.com>
+|*|
+|*|  This file is part of the XMPMusicPlayer program.
+|*|
+|*|  AvCaster is free software: you can redistribute it and/or modify
+|*|  it under the terms of the GNU Lesser General Public License version 3
+|*|  as published by the Free Software Foundation.
+|*|
+|*|  AvCaster is distributed in the hope that it will be useful,
+|*|  but WITHOUT ANY WARRANTY; without even the implied warranty of
+|*|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|*|  GNU Lesser General Public License for more details.
+|*|
+|*|  You should have received a copy of the GNU Lesser General Public License
+|*|  along with AvCaster.  If not, see <http://www.gnu.org/licenses/>.
+\*/
+
 #ifndef MACROS_H
 #define MACROS_H
 
